54a7e57e0cc9f1742e2460d8c4b029fb7dbc425f
vj-2303 <vanaraj1018@gmail.com>
2025-09-24 22:22:23 +0530

    Create post handler and page implemented

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,6 +9,9 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/paste/view/:id", app.pasteView)
 
